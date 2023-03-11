b099ff41917574d35d032f1b472a0bd0030398c3
abdelmoamen <moaammmeen@gmail.com>
2023-03-11 19:49:24 +0200

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+argv_len = len(argv)
+i = 1
+if argv_len > 1:
+    print("{} arguments:".format(argv_len - 1))
+    while i < argv_len:
+     print("{}:".format(i),argv[i])
+     i+=1
+else:
+    print("0 arguments.")
+
