50b1259a12f98371cd35d1da0eed933bbf241c5c
abdelmoamen <moaammmeen@gmail.com>
2023-03-25 22:21:15 +0200

    only diff

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,6 @@ def only_diff_elements(set_1, set_2):
     new_set = set(set_1).union(set_2)
     for i in set_1:
         for x in set_2:
-             if i == x:
+            if i == x:
                 new_set.remove(i)
     return new_set
