895a54ede68ee291f9b21d785fd95bcf06e52e08
abdelmoamen <moaammmeen@gmail.com>
2023-03-26 00:11:27 +0200

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    """return none if dict in empty"""
+    if a_dictionary is None:
         return None
     max_val = max(a_dictionary.values())
     for key in a_dictionary:
