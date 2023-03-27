16c9c41ae6d521a3335dcb17f5330e714af22786
abdelmoamen <moaammmeen@gmail.com>
2023-03-27 20:15:19 +0200

    initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 1
+    while True:
+        try:
+            print(*my_list[:x])
+            i+=1
+            return i
+        except (IndexError):
+            return
