c6edf066d8af92fc27188289461cfc226407dc62
abdelmoamen <moaammmeen@gmail.com>
2023-03-27 20:16:05 +0200

    initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while True:
         try:
             print(*my_list[:x])
-            i+=1
+            i + = 1
             return i
-        except (IndexError):
+        except IndexError:
             return
