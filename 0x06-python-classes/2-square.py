99166aa38abe5e78b098653f8c3a8af1aa117f62
abdelmoamen <moaammmeen@gmail.com>
2023-03-28 21:51:23 +0200

    raise some exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""create a class"""
+
+
+class Square:
+    """define size as private attr"""
+    def __init__(self, size=0):
+        """check if the size is not integer"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+            """check if the size is less than 0"""
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
