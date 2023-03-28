9abaae696d270a5e1e217ea497af3255b10e1e88
abdelmoamen <moaammmeen@gmail.com>
2023-03-28 22:04:11 +0200

    get the area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,5 +12,6 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+    """area method that return area by size power of 2"""
     def area(self):
         return self.__size ** 2
