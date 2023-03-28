ab59a076f86203bc3db11d092006254056f9d2b7
abdelmoamen <moaammmeen@gmail.com>
2023-03-28 22:23:34 +0200

    getter and setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,9 +7,11 @@ class Square:
     def __init__(self, size=0):
         """check if the size is not integer"""
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,5 +21,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     """area method that return area by size power of 2"""
+
     def area(self):
         return self.__size ** 2
