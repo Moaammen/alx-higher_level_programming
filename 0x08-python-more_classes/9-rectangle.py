3f04cd055f2464eeae7987a146240fbe7dec7e83
abdelmoamen <moaammmeen@gmail.com>
2023-04-05 20:53:25 +0200

    new class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -73,7 +73,7 @@ class Rectangle:
         """class method that return a new rectangle
           that size == width == height
         """
-        return Rectangle(size, size))
+        return Rectangle(cls(size, size))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
