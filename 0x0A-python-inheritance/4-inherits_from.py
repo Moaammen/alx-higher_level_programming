dd350b3fb6e2bbad8d09468927e0be75a9babd0b
abdelmoamen <moaammmeen@gmail.com>
2023-04-11 22:33:49 +0200

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""single function"""
+
+
+def inherits_from(obj, a_class):
+    """return true if its inherit from class"""
+
+    return ((issubclass(obj, a_class)) and type(obj) != a_class)
