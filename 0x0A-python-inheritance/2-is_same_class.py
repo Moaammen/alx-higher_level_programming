e3a1bb367223d37b934047e82fa350985fbe121b
abdelmoamen <moaammmeen@gmail.com>
2023-04-11 22:23:49 +0200

    is same class ?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""function check if the object is in class or not"""
+
+
+def is_same_class(obj, a_class):
+    """return true is type of obj == a_class"""
+
+    return obj == a_class
