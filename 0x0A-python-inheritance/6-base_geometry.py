0433a041b77d73581d9e9c4c262c340fa7f45a3f
abdelmoamen <moaammmeen@gmail.com>
2023-04-11 22:40:39 +0200

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""initial class"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+
+    """Public instance method"""
+    def area(self):
+        raise ValueError("area() is not implemented")
