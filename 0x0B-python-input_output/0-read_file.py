f2c68094c7c5abcf3dbd482c5cfd92a3df67e5f6
abdelmoamen <moaammmeen@gmail.com>
2023-04-12 21:31:01 +0200

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""single function"""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read())
